2b9c84ac11542e7a0f7d94a724ddcc9e0d78eff6
Georgia Guirguis <georgia.guirguis@shift.com>
2025-01-15 11:00:28 -0800

    first demo test

---
+++ PlaywrightDemo/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightDemo;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        //Playwright
+        using var playwright = await Playwright.CreateAsync();
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions{ Headless = false});
+        //Page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync($"http://www.eaapp.somee.com/");
+        await page.ClickAsync(selector: "text=Login");
+        await page.ScreenshotAsync(new PageScreenshotOptions {Path = "EAApp.jpg" });
+
+    }
+}
