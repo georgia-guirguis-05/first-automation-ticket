5858d75baa551b4c4f0b998d3246b9248c6b272d
Georgia Guirguis <georgia.guirguis@shift.com>
2025-01-15 11:38:38 -0800

    running paralell tests

---
+++ PlaywrightNUnitDemo/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightNUnitDemo;
+[Parallelizable(ParallelScope.Self)] 
+public class UnitTest2 : PageTest
+{
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync("http://eaapp.somee.com/");
+    }
+
+    [Test]
+    public async Task MyTest()
+    {
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await Page.GetByLabel("UserName").ClickAsync();
+        await Page.GetByLabel("UserName").FillAsync("admin");
+        await Page.GetByLabel("Password").ClickAsync();
+        await Page.GetByLabel("Password").FillAsync("password");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Employee List" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
+        Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Employee" }));
+    }
+}
